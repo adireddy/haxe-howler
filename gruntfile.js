6a9efd24a3917ce62bb8299927ea5126dd2ea221
Adi Mora <adi.mora@gamesys.co.uk>
2015-01-01 17:47:17 +0000

    added copy

--- gruntfile.js
+++ gruntfile.js
@@ -14,10 +14,15 @@ module.exports = function (grunt) {
 
         zip: {
             "howler.zip": ["howler/**", "haxelib.json"]
+        },
+
+        exec: {
+            copy: "cp -R samples/_output/** ../adireddy.github.io/demos/haxe-howler/"
         }
     });
 
     grunt.loadNpmTasks("grunt-haxe");
     grunt.loadNpmTasks("grunt-zip");
-    grunt.registerTask("default", ["haxe", "zip"]);
+    grunt.loadNpmTasks("grunt-exec");
+    grunt.registerTask("default", ["haxe", "zip", "exec"]);
 };
