55d556c8262633982f4d1a069236d00ec3ba410d
Shaylin Chetty <chtsha024@myuct.ac.za>
2017-07-31 10:13:05 +0200

    Added spatial functions to howl

--- src/howler/Howler.hx
+++ src/howler/Howler.hx
@@ -3,51 +3,50 @@ package howler;
 import haxe.extern.EitherType;
 @:native("Howler")
 extern class Howler {
-
     /**
-	 * Initialize the global Howler object.
-	 * @return {Howler}
-	 */
+     * Initialize the global Howler object.
+     * @return {Howler}
+     */
     static function init():Howler;
 
     /**
-	 * Set to false to disable the auto iOS enabler.
-	 */
+     * Set to false to disable the auto iOS enabler.
+     */
     static var iOSAutoEnable:Bool;
 
     /**
-	 * No audio is available on this system if this is set to true.
-	 */
+     * No audio is available on this system if this is set to true.
+     */
     static var noAudio:Bool;
 
     /**
-	 * This will be true if the Web Audio API is available.
-	 */
+     * This will be true if the Web Audio API is available.
+     */
     static var usingWebAudio:Bool;
 
     /**
-	 * Expose the AudioContext when using Web Audio.
-	 */
+     * Expose the AudioContext when using Web Audio.
+     */
     static var ctx:js.html.audio.AudioContext;
 
     /**
-	 * Check for codec support of specific extension.
-	 * @param  {String} ext Audio file extention.
-	 * @return {Boolean}
-	 */
+     * Check for codec support of specific extension.
+     * @param  {String} ext Audio file extention.
+     * @return {Boolean}
+     */
     static function codecs(ext:String):Bool;
 
     /**
-	 * Get/set the global volume for all sounds.
-	 * @param  {Float} vol Volume from 0.0 to 1.0.
-	 * @return {Howler/Float} Returns self or current volume.
-	 */
+     * Get/set the global volume for all sounds.
+     * @param  {Float} vol Volume from 0.0 to 1.0.
+     * @return {Howler/Float} Returns self or current volume.
+     */
     static function volume(vol:Float):EitherType<Howler, Float>;
 
     /**
-	 * Handle muting and unmuting globally.
-	 * @param {Boolean} muted Is muted or not.
-	 */
+     * Handle muting and unmuting globally.
+     * @param {Boolean} muted Is muted or not.
+     */
     static function mute(muted:Bool):Howler;
 
     /**
@@ -72,7 +71,7 @@ extern class Howler {
      * @param  {Number} z The z-position of the listener.
      * @return {Howler/Array}   Self or current listener position.
      */
-    static function pos(x:Float, y:Float, z:Float):EitherType<HowlerSpatial, Array<Float>>;
+    static function pos(x:Float, y:Float, z:Float):EitherType<Howler, Array<Float>>;
 
     /**
     * Get/set the direction the listener is pointing in the 3D cartesian space.
@@ -88,5 +87,5 @@ extern class Howler {
     * @param  {Number} zUp The z-orientation of the top of the listener.
     * @return {Howler/Array}     Returns self or the current orientation vectors.
     */
-    static function orientation(x:Float, y:Float, z:Float, xUp:Float, yUp:Float, zUp:Float):EitherType<HowlerSpatial, Array<Float>>;
+    static function orientation(x:Float, y:Float, z:Float, xUp:Float, yUp:Float, zUp:Float):EitherType<Howler, Array<Float>>;
 }
